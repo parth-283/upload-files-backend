9bf115d66614095123c4815327a2e33d8398ddc5
parth-283 <kp2832002@gmail.com>
2022-07-04 18:17:10 +0530

    backEnd Files

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const fileupload = require("express-fileupload");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+app.use(fileupload());
+app.use(express.static("files"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/upload", (req, res) => {
+  const newpath = __dirname + "/files/";
+  const file = req.files;
+  const filename = file.files.name;
+  console.log("filename###############################", filename);
+
+  file.files.mv(`${newpath}${filename}`, (err) => {
+    if (err) {
+      return res
+        .status(500)
+        .send({ message: "File upload failed", code: 200, error: err });
+    }
+    return res.status(200).send({ message: "File Uploaded", code: 200, FileURL:`http://localhost:${PORT}/${filename}` });
+  });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running successfully on ${PORT}`);
+});
